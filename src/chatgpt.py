f3c4812c6e7239a190257b7ad9f08a53b035b5b2
haboubacart <haboubacart@gmail.com>
2024-05-01 18:05:36 +0200

    update

--- src/chatgpt.py
+++ src/chatgpt.py
@@ -6,12 +6,40 @@ load_dotenv()
 client = OpenAI(api_key=os.getenv('OPENAI_API_KEY'))
 
 def response_to_query(query):
-    prompt = '''Tu es un assitant, et tu t'appelles Habou Assist. 
-              tu dois repondre a la requete de l'utilsateur, comment resumer un sujet en quelques mots : 
+    prompt = '''
+              Tu es un assitant, et tu t'appelles Habou Assist. 
+              Tu dois traiter la requete de l'utilisateur et lui repondre. 
+              Tu dois être synthetique dans tes reponses.
+              Plusieurs cas d'usages existe.\n
+              
+              1- Si la requete concerne une demande de programmer d'un apprentissage, tu dois repondre avec un json comme ça :\n
               {
-                "action" : "defaut",
-                "reponse" : "ta reponse"
+                "action" : "schedule_learning",
+                "reponse" : "ta reponse pour indiquer à l'utilisateur que sa demande a été traitée",
+                "titre" : "le titre du sujet",
+                "description" : "les points essetiels du sujet accompagner des liens webs",
+                "duree" : "le nombre d'heure necessaire pour parcourir le sujet, doit etre au maximum egal à 3"
+              }\n
+              description doit être donnée avec une mise en forme html\n
+              duree est un entier positif.
+
+              \n\n
+
+              2 - Si la requete concerne une demande de resumer un sujet, tu dois repondre avec un json comme ça :\n
+              tu dois repondre a la requete de l'utilsateur, comment resumer un sujet en quelques mots : \n
+              {
+                "action" : "summarize",
+                "reponse" : "le resume du sujet que tu as realisé"
               }
+
+              \n\n
+
+              3- Si la requete est une salutation ou demande sur tes competences, tu dois repondre avec un json comme ça :\n
+              {
+                "action" : "general",
+                "reponse" : "ta reponse la plus approprie"
+              }
+
             '''
     response = client.chat.completions.create(
         model="gpt-4",
--- src/google_calandar.py
+++ src/google_calandar.py
@@ -17,17 +17,17 @@ SCOPES = ["https://www.googleapis.com/auth/calendar"]
 
 def get_creds():
   creds = None
-  if os.path.exists("../gcp_client_secret_v2.json"):
-    creds = Credentials.from_authorized_user_file("../gcp_client_secret_v2.json", SCOPES)
+  if os.path.exists("./gcp_client_secret_v2.json"):
+    creds = Credentials.from_authorized_user_file("./gcp_client_secret_v2.json", SCOPES)
   if not creds or not creds.valid:
     if creds and creds.expired and creds.refresh_token:
       creds.refresh(Request())
     else:
       flow = InstalledAppFlow.from_client_secrets_file(
-          "../credentials.json", SCOPES
+          "./credentials.json", SCOPES
       )
       creds = flow.run_local_server(port=0)
-    with open("../gcp_client_secret_v2.json", "w") as token:
+    with open("./gcp_client_secret_v2.json", "w") as token:
       token.write(creds.to_json())
   return creds
 
